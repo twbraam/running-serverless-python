1bae4d7ca36c1263d070e1ea8a7e47bac8f8569f
Thomas Braam <thomas.braam@protonmail.com>
2021-02-14 21:01:38 +0100

    .gitignore fix

--- .venv/lib/python3.8/site-packages/aws/libaws/service.py
+++
@@ -1,9 +0,0 @@
-class BaseService(object):
-    def __init__(self, settings):
-        self.settings = settings
-
-    def regions(self, *args, **kwargs):
-        raise NotImplementedError('.regions() not implemented.')
-
-    def list(self, *args, **kwargs):
-        raise NotImplementedError('.list() not implemented.')
